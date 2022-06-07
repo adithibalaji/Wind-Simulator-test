239a184af34accf4970b637cb8f7a68136296295
adithibalaji <106688613+adithibalaji@users.noreply.github.com>
2022-06-07 14:16:07 -0700

    Rewrite as class/method functionality

--- wind_test_V2.py
+++ wind_test_V2.py
@@ -15,6 +15,7 @@ LON_W = -128.5
 STD_NOW = 2.0
 STD_FORE = 3.2
 
+HEIGHTS = ['10m','20m','40m','60m']
 
 def rng(seed):
     #Initialize a random number generator based on seed
@@ -27,20 +28,43 @@ def calc_array_size(lat_min,lat_max,lon_min,lon_max, res):
     lonrange = abs(lon_max - lon_min)/res
     return [int(latrange), int(lonrange)]
 
-def generate_grid(lat_min,lat_max,lon_min,lon_max, res, std):
+
+
+class windGrid2D:
+    #Class containing both u and v direction wind data for one atmospheric height wihtin a given region
+    def __init__(self,lat_min,lat_max,lon_min,lon_max, res, std):
     #Generate grids for u and v direction winds based on input coordinates, resolution and standard devition 
     #of the i=wind strength
-    size = calc_array_size(lat_min,lat_max,lon_min,lon_max, res)
-    rand1 = rng(SEED)
-    rand2 = rng(SEED)
-    ugrid = rand1.normal(0,std,size)
-    vgrid = rand2.normal(0,std,size)
-    return ugrid,vgrid
+        size = calc_array_size(lat_min,lat_max,lon_min,lon_max, res)
+        rand1 = rng(SEED)
+        rand2 = rng(SEED+1)
+        self.ugrid = rand1.normal(0,std,size)
+        self.vgrid = rand2.normal(0,std,size)
+
+    def __repr__(self):
+        return f'u-grid: {self.ugrid}, v-grid: {self.vgrid}' 
+
+class windGrid3D:
+    #Class containing  dictionnary of windGrid2D objects for each given height
+    def __init__(self,lat_min,lat_max,lon_min,lon_max, res, std, heights):
+        self.altitudes = {}
+        seed = SEED
+        for height in heights:
+            self.altitudes[height] = windGrid2D(lat_min, lat_max, lon_min, lon_max, res, std)
+            #Locally change seed value to get different values for each altitude's wind data
+            #set to 2 and not 1 so there is no overlap between current level v-grid and next level u-grid
+            seed += 2
+        
+    def __repr__(self):
+        return str(self.altitudes)
+
+            
+
+            
 
 
 
 #Sample grids for Vancouver Island coordinates for one level of wind
-wind_now = generate_grid(LAT_S,LAT_N,LON_E,LON_W,RESOLUTION,STD_NOW)
-wind_fore = generate_grid(LAT_S,LAT_N,LON_E,LON_W,RESOLUTION,STD_FORE)
-print(wind_now)
-print(wind_fore)
+#wind_now = windGrid2D(LAT_S,LAT_N,LON_E,LON_W,RESOLUTION,STD_NOW,HEIGHTS)
+wind_fore = windGrid3D(LAT_S,LAT_N,LON_E,LON_W,RESOLUTION,STD_FORE, HEIGHTS)
+wind_now = windGrid3D(LAT_S,LAT_N,LON_E,LON_W,RESOLUTION,STD_NOW, HEIGHTS)
